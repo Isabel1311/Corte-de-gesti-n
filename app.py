293addab746bf89a6432e235f498c06fd6fa9c73
Isabel Díaz <isabel.diaz2219@gmail.com>
2025-05-22 17:06:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if uploaded_file:
         )
         prov_grp["% En Tiempo"] = (prov_grp["En_Tiempo"] / prov_grp["Ordenes"] * 100).round(1)
         prov_grp["% Fuera Tiempo"] = (prov_grp["Fuera_Tiempo"] / prov_grp["Ordenes"] * 100).round(1)
-        prov_grp = prov_grp.sort_values("Ordenes", ascending=False).head()
+        prov_grp = prov_grp.sort_values("Ordenes", ascending=False).head(30)
         st.dataframe(prov_grp)
 
         fig_prov = px.bar(prov_grp, x=prov_grp.index, y=["% En Tiempo", "% Fuera Tiempo"],
